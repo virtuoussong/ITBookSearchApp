fd8801cafba1630d49ab2842f4cd590e77aad279
Chiman <chimasong@gmail.com>
2021-03-30 23:56:10 +0900

    Initial Commit

---
+++ sendBird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sendBird
+//
+//  Created by chiman song on 2021/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
